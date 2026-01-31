6601e642c7682533af4865523815c5d4ed8ea925
aforAp <satheeshsubbu1612@gmail.com>
2026-01-31 11:52:32 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer';
+import { useAppContext } from './context/AppContext';
+import Login from './components/Login';
+import AllProducts from './pages/AllProducts';
+import ProductCategory from './pages/ProductCategory';
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
+import AddAddress from './pages/AddAddress';
+import MyOrders from './pages/MyOrders';
+const App = () => {
+  //Now it will be displayes in the user dashboard only not in seller dashboard to confirm this 
+  //we can use the location and see if it is there or not
+  const isSellerPath = useLocation().pathname.includes('/seller');
+  const {showUserLogin} = useAppContext();
+  return (
+    <div>
+      {isSellerPath ? null : <NavBar />}
+      {showUserLogin ? <Login /> : null}
+      <Toaster />
+      <div className={`${isSellerPath ? "" : 'px-6 md:px-16 lg:px-24 xl:px-32'}`}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<AllProducts />} />
+          <Route path='/products/:category' element={<ProductCategory />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path="/add-address" element={<AddAddress />} />
+        <Route path='/products/:category/:id' element={<ProductDetails />} />
+        <Route path="/my-orders" element={<MyOrders />} />
+        </Routes>
+      </div>
+      {
+
+      !isSellerPath && <Footer />
+      }
+    </div>
+  )
+}
+
+export default App;
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,90 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children}) => {
+    const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate();
+    const [user, setUser] = useState(true);
+    const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [cartItems, setCartItems] = useState({});
+const [searchQuery, setSearchQuery] = useState("");
+const [count, setCount] = useState(0);
+
+    //Fetch All products
+    const fetchProducts = async () => {
+    setProducts(dummyProducts);
+    }
+    
+
+    const addToCart = (itemId) => {
+        let cartData = structuredClone(cartItems);
+        if(cartData[itemId]) {
+            cartData[itemId] += 1;
+        } else {
+            cartData[itemId] = 1;
+        }
+        setCartItems(cartData);
+        toast.success("Item added to cart");
+    }
+    //update cart item Quantity
+    const updateCartItem = (itemId, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId] = quantity;
+        setCartItems(cartData);
+        toast.success("Cart updated successfully");
+    }
+    const removeFromCart = (itemId) => {
+        let cartData = structuredClone(cartItems);
+        if(cartData[itemId]) {
+            cartData[itemId] -= 1;
+            if(cartData[itemId] === 0) {
+                delete cartData[itemId];
+            }
+    }
+    toast.success("Item removed from cart");
+    setCartItems(cartData);
+}
+
+//Get Cart item count
+const getCartCount = () => {
+    let totalCount = 0;
+    for (const item in cartItems) {
+        totalCount += cartItems[item];
+
+    }
+    return totalCount;
+}
+
+//Get Cart Total Amount
+
+const getCartAmount = () => {
+    let totalAmount = 0;
+    for(const items in cartItems) {
+        let itemInfo = products.find((product) => product._id === items);
+        if(cartItems[items] > 0) {
+            totalAmount += itemInfo.offerPrice * cartItems[items]
+        }
+
+    }
+    return Math.floor(totalAmount * 100) / 100;
+}
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    const value = {navigate, count, setCount, getCartAmount, getCartCount, searchQuery, setSearchQuery, products, user, cartItems, removeFromCart, setUser, updateCartItem,setIsSeller, isSeller, showUserLogin, setShowUserLogin, currency, addToCart};
+
+ return <AppContext.Provider value={value}>
+    {children}
+ </AppContext.Provider>
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext);
+}
