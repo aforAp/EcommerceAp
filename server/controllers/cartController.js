d982b6e645e9146619e0bad019ded15429abe2d7
aforAp <satheeshsubbu1612@gmail.com>
2026-02-01 20:02:24 +0530

    backend files

---
+++ server/controllers/cartController.js
@@ -0,0 +1,17 @@
+//Update User CartData : /api/cart/update
+
+import User from "../models/User.js"
+
+export const updateCart = async () => {
+    try {
+const {userId, cartItems} = req.body
+await User.findByIdAndUpdate(userId, {cartItems})
+res.json({
+    success: true,
+    message: "Cart Updated"
+})
+    } catch (error) {
+console.log(error.message);
+res.json({success: false, message: error.message})
+    }
+}
---
+++ server/controllers/orderController.js
@@ -0,0 +1,73 @@
+//Place Order COD: /api/order/cod
+
+import Order from "../models/Order.js";
+import Product from "../models/Product.js";
+
+export const placeOrderCOD = async (req, res) => {
+try {
+  const {userId, items, address} = req.body;
+  if(!address || items.length === 0) {
+    return res.json({
+        success: false,
+         message: "Invalid Data"
+    })
+  }
+  //Calculate Amount using Items
+  let amount = await items.reduce(async (acc , item) => {
+     const product = await Product.findById(item.product);
+     return (await acc) + product.offerPrice * item.quantity;
+
+  }, 0);
+
+  //Add Tax Charge (2%)
+  amount += Math.floor(amount * 0.02);
+
+  await Order.create({
+    userId,
+    items,
+    amount,
+    address,
+    paymentType: "COD",
+  });
+
+  return res.json({
+    success: true,
+    message: "Order placed Successfully"
+  })
+} catch (error) {
+  return res.json({success: false, message: error.message})
+}
+}
+
+//Get Orders by User Id : /api/order/user
+
+export const getUserOrders = async (req, res) => {
+  try {
+  const {userId} = req.body;
+  const orders = await Order.find({
+    userId,
+    $or: [{paymentType: "COD"}, {isPaid: true}]
+    //whenever is paymentType is online and isPaid is false it will not display to the user
+}).populate("items.product address").sort({
+    createdAt: -1
+    //-1 will display newly added order to the top
+});
+res.json({success: true, orders});
+  } catch (error) {
+res.json({success: false, message: error.message});
+  }
+}
+
+//Get All Orders (for seller /admin) : /api/order/seller
+
+export const getAllOrders = async (req, res) => {
+    try {
+        const orders = await Order.find({
+            $or: [{paymentType: "COD"}, {isPaid: true}]
+        }).populate("items.product address");
+        res.json({
+            success: true,
+             o
+        })
+    }
+}
